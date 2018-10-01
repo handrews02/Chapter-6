be7371798f74e7ea94f7069d61633b5fa16664dc
Hannah Andrews <handrews02@users.noreply.github.com>
2018-10-01 09:48:41 -0500

    updated text

---
+++ Chapter-6/src/Exercises/TestScoreStatistics.java
@@ -0,0 +1,13 @@
+package Exercises;
+
+public class TestScoreStatistics {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int userEntry;
+		int count;
+		count total;
+	}
+
+}
